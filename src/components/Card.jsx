3e6d22d0156ccd9b6c36395be701ccc24cfb9139
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-06-09 20:44:40 -0600

    Cleans some code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import starIcon from '../../public/star.svg'
 import './Card.css'
 
-function Card({img, rating, reviewCount, location, title, price, openSpots}) {
+function Card(props) {
+    const {coverImg, stats, location, title, price, openSpots} = props
+    
     let badgeText;
     if( openSpots === 0 ) {
         badgeText = 'SOLD OUT'
@@ -14,13 +16,13 @@ function Card({img, rating, reviewCount, location, title, price, openSpots}) {
     return(
         <div className='card'>
             {badgeText && <div className='card__badge'>{badgeText}</div>}
-            <img className='card__img' src={`../../public/${img}`} />
+            <img className='card__img' src={`../../public/${coverImg}`} />
             <div className='card__stats margin'>
                 <img className='stats__star' src={starIcon} alt="star" />
-                <p className='card__text'>{rating}
+                <p className='card__text'>{stats.rating}
                 </p>
                 <p className='card__text card__text--grey'>
-                {`(${reviewCount}) •${location}`}</p>
+                {`(${stats.reviewCount}) •${location}`}</p>
             </div>
             <p className='card__title card__text'>{title}</p>
             <p className='card__text'><span className='card__text--bold'>From ${price}</span> / person</p>
